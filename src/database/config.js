e962a5766cf3a38301bb59686606ee2e80817772
RavindradeJesus <ravindramandu@hotmail.com>
2022-06-05 00:04:14 -0300

    insere

--- src/database/config.js
+++ src/database/config.js
@@ -19,7 +19,7 @@ var sql = require('mssql');
 
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
    var mySqlConfig = {
-    host: "localhost:3306",
+    host: "localhost",
     user: "root",
     database: "Imusica",
     password: "#Gf03143756276",
